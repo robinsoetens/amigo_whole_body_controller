14524233955631b4823cc5e34e339d7aa297c60d
Ramon Wijnands <rayman747@hotmail.com>
2014-06-05 15:37:03 +0000

    improve force plotter
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/user/Ramon/amigo_whole_body_controller@24885 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- plot.py
+++ plot.py
@@ -4,8 +4,20 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-fname = "out.txt"
+from optparse import OptionParser
 
+usage = "usage: %prog [options] arg"
+
+parser = OptionParser()
+parser = OptionParser(usage=usage)
+parser.add_option("-l", "--link", dest="link", default="fore_arm_left")
+
+(options, args) = parser.parse_args()
+if len(args) != 1:
+	parser.print_usage(file=None)
+	exit()
+
+fname = args[0]
 data = [];
 start_time = None
 
@@ -26,11 +38,13 @@ with open(fname) as f:
 				'time':	t,
 				'name': a[2],
 
+				# pointOnA
 				'p0':	a[3],
 				'p1':	a[4],
 				'p2':	a[5],
 				'p': [a[3], a[4], a[5]],
 
+				# direction
 				'n0':	a[6],
 				'n1':	a[7],
 				'n2':	a[8],
@@ -38,11 +52,14 @@ with open(fname) as f:
 			}
 			data.append(d)
 
+time_min = min([d['time'] for d in data])
+time_max = max([d['time'] for d in data])
+
 data_bt   = [d for d in data if d['type'] == 'rp_bt']
 data_temp = [d for d in data if d['type'] == 'rp_temp']
 data_fcl  = [d for d in data if d['type'] == 'rp_fcl']
 
-link = 'fore_arm_left'
+link = options.link
 
 t_bt  = [d['time'] for d in data_bt if d['name'] == link]
 p_bt  = [d['p']    for d in data_bt if d['name'] == link]
@@ -55,9 +72,17 @@ p_fcl  = [d['p']    for d in data_fcl if d['name'] == link]
 
 axis = plt.subplot(3,1,1)
 plt.plot(t_bt, p_bt)
+plt.xlim(time_min, time_max)
+plt.legend(['x', 'y', 'z'])
+
 axis = plt.subplot(3,1,2)
 plt.plot(t_temp, p_temp)
+plt.xlim(time_min, time_max)
+plt.legend(['x', 'y', 'z'])
+
 axis = plt.subplot(3,1,3)
 plt.plot(t_fcl, p_fcl)
+plt.xlim(time_min, time_max)
+plt.legend(['x', 'y', 'z'])
 
 plt.show()
