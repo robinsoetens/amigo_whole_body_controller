9f289bac77858f63f689ad0817b8464e221b60c8
Ramon Wijnands <rayman747@hotmail.com>
2014-09-22 17:36:29 +0200

    implement threading

--- src/collision_detection/edworld.cpp
+++ src/collision_detection/edworld.cpp
@@ -1,11 +1,15 @@
 #include "edworld.h"
 
 #include <fcl/broadphase/broadphase_dynamic_AABB_tree.h>
+#include <ros/init.h>
+#include <ros/console.h>
 
 namespace wbc {
 
-EdWorld::EdWorld()
+EdWorld::EdWorld() :
+    rate_(10)
 {
+
 }
 
 void EdWorld::initialize()
@@ -14,15 +18,25 @@ void EdWorld::initialize()
     update();
 }
 
-boost::shared_ptr<fcl::BroadPhaseCollisionManager> EdWorld::getCollisionManager() const
+void EdWorld::loop()
 {
-    // TODO: locking
-    return world_;
+    while (ros::ok())
+    {
+        ROS_INFO("ed update");
+        update();
+        rate_.sleep();
+    }
 }
 
 void EdWorld::start()
 {
-    // TODO: start a thread
+    thread_ = boost::thread(&EdWorld::loop, this);
+}
+
+boost::shared_ptr<fcl::BroadPhaseCollisionManager> EdWorld::getCollisionManager() const
+{
+    // TODO: locking
+    return world_;
 }
 
 void EdWorld::update()
--- src/collision_detection/edworld.h
+++ src/collision_detection/edworld.h
@@ -3,23 +3,34 @@
 
 #include "world.h"
 #include <ed_wbc/ed_client.h>
+#include <boost/thread.hpp>
+#include <ros/rate.h>
 
 namespace wbc {
 
 class EdWorld : public World
 {
   public:
+
     EdWorld();
 
     void initialize();
 
-    boost::shared_ptr<fcl::BroadPhaseCollisionManager> getCollisionManager() const;
-
     void start();
+
+    boost::shared_ptr<fcl::BroadPhaseCollisionManager> getCollisionManager() const;
 
   protected:
 
+    ros::Rate rate_;
+
+    void loop();
+
     void update();
+
+    boost::thread thread_;
+
+    boost::mutex mtx_;
 
     boost::shared_ptr<fcl::BroadPhaseCollisionManager> world_;
 
