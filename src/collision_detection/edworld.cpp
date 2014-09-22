dd517934499049504914706bf8dfe4ff77e5cc72
Ramon Wijnands <rayman747@hotmail.com>
2014-09-22 15:16:45 +0200

    implementent all the basic functionality

--- src/collision_detection/edworld.cpp
+++ src/collision_detection/edworld.cpp
@@ -1,9 +1,34 @@
 #include "edworld.h"
 
+#include <fcl/broadphase/broadphase_dynamic_AABB_tree.h>
+
 namespace wbc {
 
 EdWorld::EdWorld()
 {
+}
+
+void EdWorld::initialize()
+{
+    client_.initialize();
+    update();
+}
+
+boost::shared_ptr<fcl::BroadPhaseCollisionManager> EdWorld::getCollisionManager() const
+{
+    // TODO: locking
+    return world_;
+}
+
+void EdWorld::start()
+{
+    // TODO: start a thread
+}
+
+void EdWorld::update()
+{
+    fcl::BroadPhaseCollisionManager *world = client_.getWorld();
+    world_ = boost::shared_ptr<fcl::BroadPhaseCollisionManager>(world);
 }
 
 } // namespace
--- src/collision_detection/edworld.h
+++ src/collision_detection/edworld.h
@@ -2,13 +2,29 @@
 #define EDWORLD_H
 
 #include "world.h"
+#include <ed_wbc/ed_client.h>
 
 namespace wbc {
 
 class EdWorld : public World
 {
-public:
+  public:
     EdWorld();
+
+    void initialize();
+
+    boost::shared_ptr<fcl::BroadPhaseCollisionManager> getCollisionManager() const;
+
+    void start();
+
+  protected:
+
+    void update();
+
+    boost::shared_ptr<fcl::BroadPhaseCollisionManager> world_;
+
+    ed_wbc::EdClient client_;
+
 };
 
 } // namespace
