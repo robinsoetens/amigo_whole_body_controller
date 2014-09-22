f531272c042915bdc3266450aa0c94c3afe39429
Ramon Wijnands <rayman747@hotmail.com>
2014-09-22 14:48:31 +0200

    add an ed collision world

---
+++ src/collision_detection/edworld.cpp
@@ -0,0 +1,9 @@
+#include "edworld.h"
+
+namespace wbc {
+
+EdWorld::EdWorld()
+{
+}
+
+} // namespace
---
+++ src/collision_detection/edworld.h
@@ -0,0 +1,14 @@
+#ifndef EDWORLD_H
+#define EDWORLD_H
+
+namespace wbc {
+
+class EdWorld
+{
+public:
+    EdWorld();
+};
+
+} // namespace
+
+#endif // EDWORLD_H
